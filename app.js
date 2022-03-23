7bec1c3483a4dca03e08ab69d54e31ad0837f600
Greg Skiano <gregoryskiano@gmail.com>
2022-03-23 14:31:47 -0700

    testing mouse tracking

--- app.js
+++ app.js
@@ -10,21 +10,45 @@ const scale = window.devicePixelRatio; // Change to 1 on retina screens to see b
 const canvas = document.createElement('canvas');
 
 canvas.width = canvasWrapElm.clientWidth;
-canvas.height = canvasWrapElm.clientHeight - 3;
+canvas.height = canvasWrapElm.clientHeight;
 const ctx = canvas.getContext('2d', { alpha: false });
 ctx.scale(scale, scale); // Normalize coordinate system to use css pixels.
 
 canvasWrapElm.appendChild(canvas);
 
+const BRICK = 'BRICK';
+const HALF_DROP = 'HALF_DROP';
+const FULL_DROP = 'FULL_DROP';
+
+const pattern = {
+  background: null,
+  colors: {
+    A: null,
+    B: null,
+    C: null,
+  },
+  layers: [
+    {
+      size: 20,
+      mode: BRICK,
+      fill: 'A........BC.......D......DDDD......',
+    }
+  ]
+}
+
 
 let offset;
 
-function loop() {
+function loop(cursorX, cursorY) {
   const { width, height } = canvas;
 
   const imageData = ctx.getImageData(0, 0, width, height);
   const { data } = imageData;
 
+  let pxSize = 18;
+
+  let currentX = Math.floor(cursorX / pxSize);
+  let currentY = Math.floor(cursorY / pxSize);
 
   let p = 0;
   for (let i = 0; i < data.length; i += 4) {
@@ -32,12 +56,15 @@ function loop() {
     let x = p % width;
     let y = Math.floor(p / width);
 
-    const c = (Math.floor(x / 15) % 2 === 0) === (Math.floor(y / 15) % 2 === 0)
+    let px = Math.floor(x / pxSize);
+    let py = Math.floor(y / pxSize);
+
+    const c = (px % 2 === 0) === (py % 2 === 0)
       ? 240
       : 230;
 
     data[i] = c;
-    data[i + 1] = c;
+    data[i + 1] = currentX === px && currentY === py ? 0 : c;
     data[i + 2] = c;
 
     p++;
@@ -49,4 +76,8 @@ function loop() {
 }
 
 loop();
+
+canvas.addEventListener('mousemove', (e) => {
+  loop(e.layerX, e.layerY);
+});
 
